a6e7d74011f9e168a0c28f54cd7935fcd31b0bfb
Yury Morozov <morozovypa1@gmail.com>
2022-11-27 21:11:44 +0800

    added home page

---
+++ lib/src/presentation/pages/home/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/basic.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: const Center(child: Text('home page')));
+  }
+}
