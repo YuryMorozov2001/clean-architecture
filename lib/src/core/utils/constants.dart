af0bf2e9bd769dd42b2b301f74a4eb946c9db057
Yury Morozov <morozovypa1@gmail.com>
2022-12-05 20:46:17 +0800

    added core folder

--- lib/src/core/utils/constants.dart
+++ lib/src/core/utils/constants.dart
@@ -1,1 +1,6 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 const String kMaterialAppTitle = 'Flutter Clean Arch';
+
+const String kApiUrl = 'https://newsapi.org/v2/everything';
+final String? kApiKey = dotenv.env['NEWS_API_KEY'];
